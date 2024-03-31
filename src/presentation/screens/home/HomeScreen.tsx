b9525d9ad57052d3ce5f73ab410ad32d5d9ee701
JuanIgnacioPiroso <jpiroso@frba.utn.edu.ar>
2024-03-31 20:12:16 -0300

    Add logout functionality to HomeScreen

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,13 +1,17 @@
 import {Button, Icon, Layout, Text} from '@ui-kitten/components';
+import {useAuthStore} from '../../store/auth/useAuthStorage';
 
 export const HomeScreen = () => {
+  const {logout} = useAuthStore();
   return (
-    <Layout>
+    <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>HomeScreen</Text>
 
-      <Icon name="facebook" />
+      {/* <Icon name="facebook" /> */}
 
-      <Button>Cerrar sesion</Button>
+      <Button accessoryLeft={<Icon name="log-out-outline" />} onPress={logout}>
+        Cerrar sesion
+      </Button>
     </Layout>
   );
 };
