870f81f0622f2da43509bd3c6b373d654ecddf5d
JuanIgnacioPiroso <jpiroso@frba.utn.edu.ar>
2024-04-15 18:37:35 -0300

    Update HomeScreen to use react-query for fetching products

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,17 +1,25 @@
 import {Button, Icon, Layout, Text} from '@ui-kitten/components';
 import {useAuthStore} from '../../store/auth/useAuthStorage';
+import {getProductsByPage} from '../../../actions/products/get=products-by-page';
+import {useQuery} from '@tanstack/react-query';
+import {MainLayout} from '../../layouts/MainLayout';
 
 export const HomeScreen = () => {
   const {logout} = useAuthStore();
+
+  const {isLoading, data: products = []} = useQuery({
+    queryKey: ['products', 'infinite'],
+    staleTime: 1000 * 60 * 60, // 1 hour
+    queryFn: () => getProductsByPage(0),
+  });
   return (
-    <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>HomeScreen</Text>
-
-      {/* <Icon name="facebook" /> */}
-
-      <Button accessoryLeft={<Icon name="log-out-outline" />} onPress={logout}>
-        Cerrar sesion
-      </Button>
-    </Layout>
+    <MainLayout
+      title="TesloShop - Products"
+      subTitle="Aplicacion administrativa"
+      >
+        <Text>
+          Holaa
+        </Text>
+      </MainLayout>
   );
 };
