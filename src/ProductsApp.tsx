4740251c00697f162268c2d236cfd4a2d6b34f3b
JuanIgnacioPiroso <jpiroso@frba.utn.edu.ar>
2024-04-15 18:03:12 -0300

    Add @tanstack/react-query dependency and update ProductsApp to use QueryClientProvider

--- src/ProductsApp.tsx
+++ src/ProductsApp.tsx
@@ -11,6 +11,9 @@ import {StackNavigator} from './presentation/router/StackNavigator';
 import {useColorScheme} from 'react-native';
 import {EvaIconsPack} from '@ui-kitten/eva-icons';
 import {AuthProvider} from './presentation/providers/AuthProvider';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 export const ProductsApp = () => {
   const colorScheme = useColorScheme();
@@ -22,7 +25,7 @@ export const ProductsApp = () => {
       : theme['color-basic-100'];
 
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <IconRegistry icons={EvaIconsPack} />
       <ApplicationProvider {...eva} theme={theme}>
         <NavigationContainer
@@ -42,6 +45,6 @@ export const ProductsApp = () => {
           </AuthProvider>
         </NavigationContainer>
       </ApplicationProvider>
-    </>
+    </QueryClientProvider>
   );
 };
